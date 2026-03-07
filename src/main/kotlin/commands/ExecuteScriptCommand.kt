aa1758782f51777de409b24b6908054358a1889f
Lena <kadilova08@mail.ru>
2026-03-07 13:22:44 +0300

    обработка скрипта (нужно доработать) с файлом скрипта

---
+++ src/main/kotlin/commands/ExecuteScriptCommand.kt
@@ -0,0 +1,24 @@
+package commands
+
+import collection.CollectionManager
+import collection.CommandManager
+
+class ExecuteScriptCommand(
+    private val collectionManager: CollectionManager,
+    private val commandManager: CommandManager
+) : Command {
+
+    override val name = "execute_script"
+    override val description = "выполнить скрипт из файла"
+
+    override fun execution(args: List<String>) {
+
+        if (args.isEmpty()) {
+            println("Укажите файл")
+            return
+        }
+
+        val fileName = args[0]
+        collectionManager.executeScript(fileName, commandManager)
+    }
+}
