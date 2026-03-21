c2cbace6f3ad0914b7d2bb5251089a2cac4f1838
Lena <kadilova08@mail.ru>
2026-03-21 14:29:16 +0300

    Добавили тесты

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib"))
     implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("io.mockk:mockk:1.13.10")
 }
 
 tasks.test {
